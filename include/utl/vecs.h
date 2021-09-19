e70da2253318ab4fa82d9910f1a25a8b667a801c
rosidae <windows10vmstuff@gmail.com>
2021-09-19 09:57:42 -0500

    Update to vecs.h

--- include/utl/vecs.h
+++ include/utl/vecs.h
@@ -1,4 +1,8 @@
 #include <utl/includes.h>
+// Due to the use of template typedefs
+// we are unable to use pointers because to use a pointer
+// the datatype needs to be known at COMPILATION time
+
 namespace Vecs {
     template <typename T>
     bool Includes(
@@ -22,11 +26,35 @@ namespace Vecs {
     std::vector<T> Tail(
         std::vector<T> Vec
     ) {
-        std::vector<T> Vec_;
+        std::vector<T> _Vec;
         for (int I = 1; I < Vec.size(); I++)
         {
-            Vec_.push_back(Vec[I]);
+            _Vec.push_back(Vec[I]);
         }
-        return Vec_;
+        return _Vec;
     }
+    template <typename T>
+    std::vector<T> Last(
+        std::vector<T> Vec
+    ) {
+        return std::vector<T>{Vec[Vec.size()-1]};
+    }
+    template <typename T>
+    std::vector<T> Indexical_Size(
+        std::vector<T> Vec
+    ) {
+        return Vec.size()-1;
+    }
+    template <typename T>
+    int Match_Count(
+        std::vector<T>  Vec,
+        T               To_Match
+    ) {
+        int _Count = 0;
+        for(T _Val: Vec) {
+            _Count += _Val == To_Match;
+        }
+        return _Count;
+    }
+
 }
--- test/main.cpp
+++ test/main.cpp
@@ -2,8 +2,6 @@
 #include <string>
 
 int main() {
-    std::vector<int> v{1,2,3,4};
-    for(int a: Vecs::Tail(v)) {
-        std::cout << a << "\n";
-    }
+    std::vector<int> v{1,1,1,1,1,1,4,5};
+    std::cout << Vecs::Match_Count(v, 1);
 }
