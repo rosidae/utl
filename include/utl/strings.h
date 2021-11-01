fb1d1f9553f176f037b6512204fa28201a549258
rosidae <windows10vmstuff@gmail.com>
2021-10-31 22:47:07 -0500

    just got github copilot. will add more features ;)

--- include/utl/strings.h
+++ include/utl/strings.h
@@ -13,5 +13,15 @@ namespace Strings {
     ) {
         return String.size() - 1;
     }
-    
+    std::string Find_And_Replace_All(
+        std::string String,
+        std::string Find,
+        std::string Replace
+    ) {
+        std::string Result = String;
+        while (Result.find(Find) != std::string::npos) {
+            Result.replace(Result.find(Find), Find.size(), Replace);
+        }
+        return Result;
+    }
 }
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,8 @@
-#include <utl/files.h>
+#include <utl/strings.h>
 
-int main()
-{
-    Files::Create_Dir("");
+int main() {
+    std::string s = "1 1 1 2 3 4 4 3 4";
+    std::cout << s << std::endl;
+    s = Strings::Find_And_Replace_All(s, "1", "a");
+    std::cout << s << std::endl;
 }
